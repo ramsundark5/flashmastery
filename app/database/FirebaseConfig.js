7f59099fcdf822e3cc7ad19c78a7c543debb3ab6
Ramsundar Kuppusamy <ramsundark5@gmail.com>
2016-09-13 11:59:27 -0700

    firestack persistence enabled now

--- app/database/FirebaseConfig.js
+++ app/database/FirebaseConfig.js
@@ -1,21 +1,18 @@
 import Firestack from 'react-native-firestack';
 
 const configurationOptions = {
-  debug: true,
-  googleAppID: 'flashmastery'
+    databaseURL: "https://flashmastery.firebaseio.com/"
 };
-
 class FirebaseConfig{
     init(){
         this.firestack = new Firestack(configurationOptions);
-        this.firestack.on('debug', msg => console.log('Received debug message', msg));
-        /*this.firestack.database.setPersistence(true);
+        //this.firestack.on('debug', msg => console.log('Received debug message', msg));
+        this.firestack.database.setPersistence(true);
         const basicFlashCardsRef = this.firestack.database
                         .ref('flashcards')
                         .child('basic');
-        basicFlashCardsRef.keepSynced(true);*/
+        //basicFlashCardsRef.keepSynced(true);
     }
-
 }
 
 export default new FirebaseConfig();
