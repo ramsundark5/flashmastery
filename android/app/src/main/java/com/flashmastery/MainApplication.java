0effa9664b15f3f12f82691c8cd074b832fc8441
Ramsundar Kuppusamy <ramsundark5@gmail.com>
2016-09-14 08:17:16 -0700

    remove firestack and use realm

--- android/app/src/main/java/com/flashmastery/MainApplication.java
+++ android/app/src/main/java/com/flashmastery/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import io.fullstack.firestack.FirestackPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
             new FirestackPackage(getApplicationContext()),
             new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new VectorIconsPackage()
--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
 import Routes from './app/Routes';
-import FirebaseConfig from './app/database/FirebaseConfig';
 
 class flashmastery extends Component {
   componentDidMount(){
-     //FirebaseConfig.init();
   }
 
   render() {
