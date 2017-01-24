e682a986305c93997b7c7660884427801ec7d257
Ramsundar Kuppusamy <ramsundar@Ramsundars-MBP.T-mobile.com>
2017-01-24 12:30:46 -0800

    added sightwords json

--- app/database/LocalDatabase.js
+++ app/database/LocalDatabase.js
@@ -1,9 +1,10 @@
 import {Kindergarten} from './kindergarten/Kindergarten';
 import {Grade1} from './grade1/Grade1';
+const SightWords = require('./SightWords.json');
 
 export const LocalDatabase = [ 
     {id: "5023c338-7b87-11e6-8b77-86f30ca893d3",  name: "Kindergarten", decks: Kindergarten, custom: false}, 
-    {id: "6bea4938-7b88-11e6-8b77-86f30ca893d3", name: "1st Grade", decks: Grade1, custom: false}, 
+    {id: "6bea4938-7b88-11e6-8b77-86f30ca893d3", name: "Sight Words", decks: SightWords, custom: false}, 
     {id: "73be0b36-7b88-11e6-8b77-86f30ca893d3", name: "2nd Grade", decks: Grade1, custom: false}, 
     {id: "7abccf62-7b88-11e6-8b77-86f30ca893d3", name: "3rd Grade", decks: Grade1, custom: false}
 ];
