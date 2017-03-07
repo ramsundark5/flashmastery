73636e6114c6ab3556cd9d01fa334a1900a6763b
Ramsundar Kuppusamy <ramsundar@Ramsundars-MBP.T-mobile.com>
2017-03-07 15:51:36 -0800

    updated sample data

--- app/database/LocalDatabase.js
+++ app/database/LocalDatabase.js
@@ -1,10 +1,17 @@
 import {Kindergarten} from './kindergarten/Kindergarten';
 import {Grade1} from './grade1/Grade1';
+const AdditionFacts = require('./AdditionFacts.json');
+const DivisionFacts = require('./DivisionFacts.json');
+const MultiplicationFacts = require('./MultiplicationFacts.json');
+const PhonicsSounds = require('./PhonicsSounds.json');
 const SightWords = require('./SightWords.json');
+const SubtractionFacts = require('./SubtractionFacts.json');
 
 export const LocalDatabase = [ 
-    {id: "5023c338-7b87-11e6-8b77-86f30ca893d3",  name: "Kindergarten", decks: Kindergarten, custom: false}, 
-    {id: "6bea4938-7b88-11e6-8b77-86f30ca893d3", name: "Sight Words", decks: SightWords, custom: false}, 
-    {id: "73be0b36-7b88-11e6-8b77-86f30ca893d3", name: "2nd Grade", decks: Grade1, custom: false}, 
-    {id: "7abccf62-7b88-11e6-8b77-86f30ca893d3", name: "3rd Grade", decks: Grade1, custom: false}
+    {id: "2329cfae-f5f3-41de-abb3-ab6224d62024",  name: "Addition Facts", decks: AdditionFacts, custom: false}, 
+    {id: "10ef1626-1366-4a9c-b4ad-dfb2a247788d",  name: "Subtraction Facts", decks: SubtractionFacts, custom: false}, 
+    {id: "140736dc-5dc9-453a-ac75-5f0731d7eeb0", name: "Division Facts", decks: DivisionFacts, custom: false}, 
+    {id: "d82dd36d-203f-4cc2-b7c2-3ea722445f79", name: "Multiplication Facts", decks: MultiplicationFacts, custom: false}, 
+    {id: "ddd81a15-a1f0-447a-bd0b-ea9f8526eec1", name: "Phonics Sounds", decks: PhonicsSounds, custom: false},
+    {id: "edb14376-4207-48a9-bbca-dfab9b5a426b", name: "Sight Words", decks: SightWords, custom: false}
 ];
