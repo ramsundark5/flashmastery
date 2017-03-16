6680d7daefaf9fda5b424fb3d2217fcbcea2f092
Ramsundar Kuppusamy <ramsundar@Ramsundars-MBP.T-mobile.com>
2017-03-16 13:11:57 -0700

    added scalable font size

--- app/Root.js
+++ app/Root.js
@@ -3,6 +3,7 @@ import Routes from './Routes';
 //import codePush from "react-native-code-push";
 import BackgroundSync from './sync/BackgroundSync';
 import { setCustomText } from 'react-native-global-props';
+import normalize from './utils/NormalizeText';
 
 class Root extends Component {
 
@@ -18,9 +19,10 @@ class Root extends Component {
     }
 }
 
+//iphne 5s - 14, 6s 18, ipad 24 
 const customTextProps = { 
   style: { 
-    fontSize: 16
+    fontSize: normalize(14)
   }
 };
 export default Root;
